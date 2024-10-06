661b215e146fd8068331fce3232789b80fb0e864
ericxias <90631685+ericxias@users.noreply.github.com>
2024-10-06 13:53:26 -0400

    java version

---
+++ app/src/test/java/ecse/TodosTest.java
@@ -0,0 +1,11 @@
+package ecse;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodosTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
